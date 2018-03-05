5c29a0e6b14e8da335c95038354fe3b620efbdfb
William Dale <williambdale@gmail.com>
2018-03-05 18:34:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  last_word = array.pop 
+  array << "and #{last_word}"
+  if array.length == 2 
+    array.join(' ')
+  elsif array.length < 2
+    array 
+  else 
+    array.join(", ")
+  end 
 end
